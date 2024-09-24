a9935535496609a0682ec7c0ec04af3c6e2f985b
cmj10301 <cmj10301@gmail.com>
2024-09-25 00:12:01 +0900

    좋아요 기능 구현

--- app/list/ListItem.js
+++ app/list/ListItem.js
@@ -1,8 +1,11 @@
 'use client'
 
 import Link from "next/link"
+import { useState } from "react"
 
 export default function ListItem({result}) { 
+    console.log(JSON.parse(result).length)
+    let [좋아요, 좋아요변경] = useState(new Array(result.length).fill(0))
     console.log(result)
     return (
     <div className="list-bg">
@@ -18,8 +21,12 @@ export default function ListItem({result}) {
                         <span onClick={(e) =>{
                             fetch('/api/delete', {method : 'DELETE', body : a._id})
                             .then((r) => r.json())
-                        }}>삭제</span>
-                        <p>1월 1일</p>
+                        }}>삭제</span> <br></br>
+                        <span onClick={() => {
+                            let copy =  [...좋아요]
+                            copy[i]++
+                            좋아요변경(copy)
+                        }}>좋아요 : {좋아요[i]}</span>
                     </div>
                 )
             })
